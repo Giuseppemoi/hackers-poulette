da96633775fccc1b45584efd3b9b5fc1841885f6
Giuseppe Moi <giuseppemoi@Giuseppes-MacBook-Air.local>
2021-08-06 20:23:40 +0200

    clean code

--- assets/elements/footer.php
+++ assets/elements/footer.php
@@ -5,6 +5,7 @@
             <a href="" class="active">Nous Contacter</a>
         </nav>
     </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="assets/elements/script.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,6 +4,8 @@ include "assets/elements/function_options.php";
 include "assets/elements/post.php";
 include "assets/elements/function_reset_post.php";
 include "assets/elements/mail.php";
+include "assets/elements/fill_imput.php";
+include "assets/elements/required.php";
 ?>
 
 <div class="form">
@@ -11,54 +13,51 @@ include "assets/elements/mail.php";
         <div class="names">
             <label for="name">
                 Name : 
-                <input type="text" name="name" id="name" value="<?= (!isset($_POST["name"]) || $_POST["name"] === NULL) ? "" : $array_post["name"]; ?>">
-                <?= !empty($_POST["submit"]) ? (array_key_exists("name", $array_error) ? "<strong class='error'>" . $array_error["name"] . "</strong>" : "") : "" ?>
+                <input type="text" name="name" id="name" value="<?= $name_isset ?>">
+                <?= $require_name ?>
             </label>
             <label for="firstname">
                 Firstname : 
-                <input type="text" name="firstname" id="firstname" value="<?= (!isset($_POST["firstname"]) || $_POST["firstname"] === NULL) ? "" : $array_post["firstname"]; ?>">
-                <?= !empty($_POST["submit"]) ? (array_key_exists("firstname", $array_error) ? "<strong class='error'>" . $array_error["firstname"] . "</strong>" : "") : "" ?>
+                <input type="text" name="firstname" id="firstname" value="<?= $firstname_isset ?>">
+                <?= $require_firstname ?>
             </label>
         </div>
         <div class="gender">
             Gender : 
                 <label for="male">
                     Male
-                    <input type="radio" name="gender" id="male" value="male" 
-                        <?= (isset($_POST["gender"]) && $array_post["gender"] === 'male') ?  'checked' : "";?>>
+                    <input type="radio" name="gender" id="male" value="male" <?= $male_gender_isset ?> >
                 </label>
                 <label for="female">
                     Female
-                    <input type="radio" name="gender" id="female" value="female" 
-                        <?= (isset($_POST["gender"]) && $array_post["gender"] === 'female') ?  'checked' : "";?>>
+                    <input type="radio" name="gender" id="female" value="female" <?= $female_gender_isset ?> >
                 </label>
-                <?= !empty($_POST["submit"]) ? (array_key_exists("gender", $array_error) ? "<strong class='error'>" . $array_error["gender"] . "</strong>" : "") : "" ?>
+                <?= $require_gender ?>
         </div>
         <label for="email">
             Email : 
-            <input type="email" name="email" id="email" value="<?= !array_key_exists("email", $array_error) ? ((!isset($_POST["email"]) || $_POST["email"] === NULL) ? "" : $array_post["email"]) : "" ?>">
-            <?= !empty($_POST["submit"]) ? (array_key_exists("email", $array_error) ? "<strong class='error'>" . $array_error["email"] . "</strong>" : "") : "" ?>
+            <input type="email" name="email" id="email" value="<?= $email_isset ?>">
+            <?= $require_email ?>
         </label>
         <label for="country">
             Select your country : 
             <select name="country" id="country">
-                <option value="<?= (!isset($array_post["country"]) || $array_post["country"] === NULL) ? "" : $array_post["country"]; ?>">
-                    <?= (!isset($_POST["country"]) || $_POST["country"] === NULL) ? "Choose a country!" : $array_post["country"]; ?>
-                </option>
+                <option value="<?= $country_isset ?>"> <?= $country_option ?> </option>
                 <?php options($countries); ?>
             </select>
-            <?= !empty($_POST["submit"]) ? (array_key_exists("country", $array_error) ? "<strong class='error'>" . $array_error["country"] . "</strong>" : "") : "" ?>
+            <?= $require_country ?>
         </label>
         <label for="subject">
             Choose a subject : 
             <select name="subject" id="subject" >
+                <?= $subject_isset ?>
                 <?php options($subjects); ?>
             </select>
         </label>
         <label class="flex" for="msg">
             Your message : 
-            <textarea name="msg" id="msg" cols="30" rows="10"><?= (!isset($_POST["message"]) || $_POST["msg"] === NULL) ? "" : $array_post["message"]; ?></textarea>
-            <?= !empty($_POST["submit"]) ? (array_key_exists("message", $array_error) ? "<strong class='error'>" . $array_error["message"] . "</strong>" : "") : "" ?>
+            <textarea name="msg" id="msg" cols="30" rows="10"><?= $message_isset ?></textarea>
+            <?= $require_message ?>
         </label>
         <input id="website" name="website" type="text" value=""  />
         <input type="submit" name="submit">
